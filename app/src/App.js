6c75b35f444f387191a82be26445e8f1fa180b4f
jonathanbataire <jonathanbataire@gmail.com>
2020-12-31 21:51:12 +0300

    summary

--- app/src/App.js
+++ app/src/App.js
@@ -15,9 +15,9 @@ function App () {
         window.location.reload(true);
     }
 
-    if(!token) {
+    /*if(!token) {
         return <Login setToken={setToken} />
-    }
+    }*/
 
     return (
         <div className="App">
--- app/src/Components/Home.js
+++ app/src/Components/Home.js
@@ -1,21 +1,65 @@
-import React from 'react';
-import '../css/Home.css'
-import PaymentRegistration from './PaymentRegistration'
-import Payments from './Payments'
-import Loans from './Loans'
-import { Col, Container, Row } from 'react-bootstrap';
+import React, { useState } from 'react';
+import '../css/Home.css';
+import PaymentRegistration from './PaymentRegistration';
+import Payments from './Payments';
+import Loans from './Loans';
+import useWindowDimensions from '../Hooks/useWindowDimensions';
+
+import { Button, Col, Collapse, Container, Row } from 'react-bootstrap';
 
 function Home (){
+    const [openPay, setOpenPay] = useState(false);
+    const [openLoan, setOpenLoan] = useState(false);
+    const { width} = useWindowDimensions();
+    
+    const summaries = ()=>{
+        if (width <= 576) {
+            return (
+                <div>
+                <Collapse in={openPay}>
+                    <Col className="" sm={12}> <Payments /> </Col>
+                </Collapse>
+                <Collapse in={openLoan}>
+                <Col className="" sm={12}> <Loans /> </Col>
+                </Collapse>
+                </div>
+            );        
+        }
+        return (
+            <div>
+                <Col className="" sm={12}> <Payments /> </Col>
+                <Col className="" sm={12}> <Loans /> </Col>
+            </div>
+        );
+    }
+    
     return (
         <Container className="" >
             <Row>
-                <Col className="" xs={12} md={7}> <PaymentRegistration /> </Col>
-                <Col xs={12} md={5}>
+                <Col sm={{ order: '12' }} xs={12} md={5}>
                     <Row>
-                        <Col className="" sm={12}> <Payments /> </Col>
-                        <Col className="" sm={12}> <Loans /> </Col>
+                        <Col className="mobile">
+                            <a
+                            href='#' style={{padding:'10px'}}
+                            onClick={() => {setOpenPay(!openPay); setOpenLoan(false); }}
+                            aria-controls="collapse-payments"
+                            aria-expanded={openPay}
+                            >
+                                payments
+                            </a> 
+                            <a
+                                href='#' style={{padding:'10px'}}
+                                onClick={() => {setOpenLoan(!openLoan); setOpenPay(false); }}
+                                aria-controls="collapse-loans"
+                                aria-expanded={openLoan}
+                            >
+                                Loans
+                            </a>
+                        </Col>
+                        {summaries()}     
                     </Row>
                 </Col>
+                <Col className="" sm={{ order: '1' }} xs={12} md={7}> <PaymentRegistration /> </Col>
             </Row>
         </Container>
     );
